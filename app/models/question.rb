5123f3c9cdaacce5104dd43320190093b38f1cab
Daniel Gouveia <danshome@Daniels-MacBook-Pro.local>
2018-04-18 13:27:28 -0400

    initial commit

---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+class Question < ApplicationRecord
+  validates :title, presence: :true, length: { minimum: 20 }
+  validates :description, presence: :true, length: { minimum: 50 }
+  belongs_to :user
+end
